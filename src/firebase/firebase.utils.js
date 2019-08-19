2348c7773c6ced0fa2a18962512f487c0ecb2071
AaronHale1989 <halea02130@gmail.com>
2019-08-19 11:40:54 -0400

    added manual email and password sign up with firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
     appId: "1:1062362182474:web:5f3c81968bd98c27"
   };
 
+  export const createUserProfileDocument = async (userAuth, data) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...data
+        })
+      } catch (error) {
+        console.log('error creating user', error)
+      }
+    }
+
+    return userRef;
+  }
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
